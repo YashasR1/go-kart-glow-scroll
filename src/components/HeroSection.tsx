9f02d32fedcf038c378fcf3aeaf02b0006a1a296
nexuz-web <nexuz.in@gmail.com>
2025-09-04 17:09:30 +0530

    added corner glows

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -29,7 +29,7 @@ const HeroSection = () => {
       </div>
 
       {/* Top left corner glow */}
-      <div className="absolute top-0 left-0 w-[400px] h-[400px] rounded-full bg-red-500/50 blur-[150px] -translate-x-1/4 -translate-y-1/4 z-10 mix-blend-screen" />
+      <div className="absolute top-0 left-0 w-[400px] h-[300px] rounded-full bg-red-500/50 blur-[150px] -translate-x-1/4 -translate-y-1/4 z-10 mix-blend-screen" />
 
       {/* Bottom right corner glow */}
       <div className="absolute bottom-0 right-0 w-[250px] h-[250px] rounded-full bg-red-500/50 blur-[150px] translate-x-1/4 translate-y-1/4 z-10 mix-blend-screen" />

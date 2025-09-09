a7c939bbc33c2e704b8c5811f52eb6e198875438
nexuz-web <nexuz.in@gmail.com>
2025-09-09 18:46:50 +0530

    ggs bro

--- src/components/LocationSection.tsx
+++ src/components/LocationSection.tsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 
 const LocationSection = () => {
   return (
-    <section id="location" className="sm:py-20 px-6 bg-black pt-8 pb-2">
+    <section id="location" className="sm:py-20 px-6 bg-black pt-12 pb-2">
       <div className="max-w-7xl mx-auto">
         <motion.div
           initial={{ opacity: 0, y: 40, filter: "blur(4px)" }}
